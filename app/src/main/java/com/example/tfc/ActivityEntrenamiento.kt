df408f62ab0f064213cce120309e641d6c574af9
13Ragnaroks <stashmustard@gmail.com>
2024-02-26 20:40:04 +0100

    1.7 CRONOMETROS LISTOS Y WV CON BOTON DE MINIMIZAR

--- app/src/main/java/com/example/tfc/ActivityEntrenamiento.kt
+++ app/src/main/java/com/example/tfc/ActivityEntrenamiento.kt
@@ -2,16 +2,108 @@ package com.example.tfc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.example.tfc.clasesAuxiliares.Cronometro
 
-class ActivityEntrenamiento : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ActivityEntrenamiento : AppCompatActivity() { override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrenamiento)
 
-        val wvReproductor=findViewById<WebView>(R.id.wvReproductor)
-        val video="<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/U5lV7oPW3CA?si=AfWeRu9obD7VmI10\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
-        wvReproductor.loadData(video,"text/html","utf-8")
+    initComponentes()
+    initCronometro()
+    initListeners()
 
     }
+    private fun initListeners() {
+        btnOcultar.setOnClickListener {
+            if(wvReproductor.visibility== View.VISIBLE)
+                wvReproductor.visibility= View.GONE
+            else
+                wvReproductor.visibility= View.VISIBLE
+        }
+        btnSiguiente.setOnClickListener {
+            tvCuentaAtras.visibility=View.VISIBLE
+            cuentaAtras()
+        }
+
+        btnStart.setOnClickListener {
+            /*setImageResource no funcionaba como debia por algún motivo,aqui simplemente llamamos a los métodos del cronometro y cambiamos la imagen pause/play
+            segun nos convenga*/
+            if(!cronometro.estaIniciado()){
+                cronometro.iniciar()
+                btnStart.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_pause))
+            }else if (cronometro.estaPausado()) {
+                cronometro.reanudar()
+                btnStart.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_pause))
+            }else{
+                cronometro.pausar()
+                btnStart.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_play))
+            }
+        }
+
+
+
+    }
+    private fun initComponentes() {
+        tvCronometro = findViewById(R.id.tvCronometro)
+        tvNombreEjercicio = findViewById(R.id.tvNombreEjercicio)
+        btnOcultar = findViewById(R.id.btnOcultar)
+        wvReproductor = findViewById(R.id.wvReproductor)
+        tvEjercicio = findViewById(R.id.tvEjercicio)
+        tvRepeticiones = findViewById(R.id.tvRepeticiones)
+        btnRestarPeso = findViewById(R.id.btnRestarPeso)
+        tvPeso = findViewById(R.id.tvPeso)
+        btnSumarPeso = findViewById(R.id.btnSumarPeso)
+        btnStart = findViewById(R.id.btnStart)
+        btnSiguiente = findViewById(R.id.btnSiguiente)
+        tvCuentaAtras = findViewById(R.id.tvCuentaAtras)
+
+
+    }
+    private fun initCronometro(){
+        cronometro= Cronometro()
+        cronometro.setOnUpdateCronometro { tiempo->
+            runOnUiThread{
+                tvCronometro.text=tiempo
+            }
+        }
+        if(!cronometro.estaIniciado()){
+            cronometro.iniciar()
+        }
+    }
+    private fun cuentaAtras(){
+        cuentaAtras= Cronometro()
+        cuentaAtras.setOnUpdateCuentaAtras { tiempo->
+            runOnUiThread{
+                tvCuentaAtras.text=tiempo
+            }
+        }
+        cuentaAtras.initCuentaAtras(4000,tvCuentaAtras)
+    }
+
+
+    private lateinit var tvCronometro: TextView
+    private lateinit var tvNombreEjercicio: TextView
+    private lateinit var btnOcultar: ImageButton
+    private lateinit var wvReproductor: WebView
+    private lateinit var tvEjercicio: TextView
+    private lateinit var tvRepeticiones: TextView
+    private lateinit var tvPeso: TextView
+    private lateinit var btnRestarPeso: FloatingActionButton
+    private lateinit var btnSumarPeso: FloatingActionButton
+    private lateinit var btnStart: FloatingActionButton
+    private lateinit var btnSiguiente:AppCompatButton
+    private lateinit var tvCuentaAtras: TextView
+    private lateinit var cronometro: Cronometro
+    private lateinit var cuentaAtras: Cronometro
+
+
 }
+
---
+++ app/src/main/java/com/example/tfc/clasesAuxiliares/Cronometro.kt
@@ -0,0 +1,124 @@
+package com.example.tfc.clasesAuxiliares
+
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.TextView
+import java.util.Locale
+
+class Cronometro{
+    //Variables cronometro
+    private var tiempoInicial: Long = 0
+    private var tiempoAcumulado: Long = 0
+    private var tiempoTotal: Long = 0
+
+    //Bolean que nos ayudará a gestionar la logica de los relojes
+    private var iniciado: Boolean = false
+    private var pausado: Boolean = false
+    //permite ejecutar codigo en el hilo principal
+    private  var handler = Handler(Looper.getMainLooper())
+    //Variable funcional para actualizar el cronometro
+    private var onUpdate: ((String) -> Unit)? = null
+
+    //Funcion que se llama al actualizar el cronometro
+    fun setOnUpdateCronometro(onUpdate: ((String) -> Unit)) {
+        this.onUpdate = onUpdate
+    }
+
+
+    fun iniciar() {
+        if(!iniciado) {
+            //Asignamos tiempo inicial para recuperarlo en tiempo de entrenamiento más tarde
+            tiempoInicial = System.currentTimeMillis()
+        }
+        //Cambiamos el bolleando a true
+        iniciado = true
+        pausado= false
+        this.cicloCronometro()
+    }
+    fun pausar(){
+        if(iniciado && !pausado){
+            //Dejamos de sumar el tiempo durante la pausa
+            tiempoAcumulado+=System.currentTimeMillis()-tiempoInicial
+            iniciado=false
+            pausado=true
+        }
+    }
+
+    fun reanudar(){
+        if(!iniciado && pausado){
+            //Reanudamos el tiempo
+            tiempoAcumulado=System.currentTimeMillis()
+            iniciado=true
+            pausado=false
+            this.cicloCronometro()
+        }
+    }
+    private fun cicloCronometro() {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                if (!pausado) {
+                    //Calculamos el tiempo total para asignarlo a nuestro cronometro
+                    tiempoTotal = System.currentTimeMillis() + tiempoAcumulado - tiempoInicial
+
+                    // Calculamos el tiempo en minutos, segundos y milisegundos
+                    val milisegundos = (tiempoTotal % 1000) / 10
+                    val segundos = (tiempoTotal / 1000) % 60
+                    val minutos = (tiempoTotal / (1000 * 60)) % 60
+                    //Mandamos el timepo ya formateado al hilo principal
+                    onUpdate?.invoke(String.format(Locale.getDefault(), "%02d:%02d:%02d", minutos, segundos, milisegundos))
+
+                    // Reprogramar la ejecución de este bloque para que se ejecute nuevamente después de 10 milisegundos
+                    handler.postDelayed(this, 10)
+                }
+            }
+        }, 0)
+    }
+
+    fun estaPausado() : Boolean {
+       return pausado
+    }
+
+    fun estaIniciado(): Boolean {
+    return iniciado
+    }
+
+    private var onUpdateCuentaAtras: ((String) -> Unit)? = null
+
+    // Variables para el cronómetro regresivo
+    private var cuentaAtrasFinal: Long = 0
+    private var cuentaAtrasRestante: Long = 0 // 30 segundos
+
+
+    fun setOnUpdateCuentaAtras(onUpdateCuentaAtras:((String)->Unit)){
+        this.onUpdateCuentaAtras=onUpdateCuentaAtras
+    }
+
+    //Métodos para iniciar la cuenta atras
+    //Le pasamos el textview para gestionar aqui su visibilidad
+    fun initCuentaAtras(cuentaInicial:Long, tvCuentaAtras:TextView) {
+        this.cuentaAtrasRestante=cuentaInicial
+        cuentaAtrasFinal = System.currentTimeMillis() + cuentaAtrasRestante
+        cicloCuentaAtras(tvCuentaAtras)
+    }
+
+    // Ciclo del contador regresivo,similar al anterior,pero restando tiempo y cambiando la visibilidad de la cuenta atras
+    private fun cicloCuentaAtras(tvCuentaAtras:TextView) {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val tiempoActual = System.currentTimeMillis()
+                cuentaAtrasRestante = cuentaAtrasFinal - tiempoActual
+
+                if (cuentaAtrasRestante > 0) {
+                    val segundos = (cuentaAtrasRestante / 1000) % 60
+                    val milisegundos = (cuentaAtrasRestante % 1000) / 10
+                    onUpdateCuentaAtras?.invoke(String.format(Locale.getDefault(),"%02d:%02d",segundos,milisegundos))
+                    handler.postDelayed(this, 10)
+                } else {
+                    onUpdateCuentaAtras?.invoke("0")
+                    tvCuentaAtras.visibility= View.GONE
+                }
+            }
+        }, 10)
+    }
+}
